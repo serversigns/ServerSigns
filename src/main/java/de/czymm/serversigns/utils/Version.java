552b6d7393045f7424d26b7376501b6f922d5576
EpiCanard <epicanard@gmail.com>
2021-06-13 17:05:41 +0200

    Add support of version 1.17 of minecraft

--- src/main/java/de/czymm/serversigns/utils/Version.java
+++ src/main/java/de/czymm/serversigns/utils/Version.java
@@ -14,7 +14,8 @@ public enum Version {
     V1_13(1, 13),
     V1_14(1, 14),
     V1_15(1, 15),
-    V1_16(1, 16);
+    V1_16(1, 16),
+    V1_17(1, 17);
 
     private int major;
     private int minor;
